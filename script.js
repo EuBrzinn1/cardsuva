9994905f6d36d951e28886ba2de9fead576b4f0d
HEROBRINE <kaua.serafim.senai404@gmail.com>
2025-06-13 15:34:50 -0300

    cardsdouva
    codigo

---
+++ script.js
@@ -0,0 +1,120 @@
+function flipCard(card) {
+  card.classList.toggle('flipped'); 
+}   //CARD GIRAR
+
+
+
+//----------------------------------
+
+function saibaMais(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal").style.display = "flex";
+}
+
+function fecharModal() {
+  document.getElementById("modal").style.display = "none";
+}  //PRIMEIRO SAIBA MAIS
+
+//----------------------------------
+
+function sobre(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal2").style.display = "flex";
+}
+
+function fecharModal2() {
+  document.getElementById("modal2").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais3(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal3").style.display = "flex";
+}
+
+function fecharModal3() {
+  document.getElementById("modal3").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais4(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal4").style.display = "flex";
+}
+
+function fecharModal4() {
+  document.getElementById("modal4").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais5(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal5").style.display = "flex";
+}
+
+function fecharModal5() {
+  document.getElementById("modal5").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais6(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal6").style.display = "flex";
+}
+
+function fecharModal6() {
+  document.getElementById("modal6").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais7(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal7").style.display = "flex";
+}
+
+function fecharModal7() {
+  document.getElementById("modal7").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais8(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal8").style.display = "flex";
+}
+
+function fecharModal8() {
+  document.getElementById("modal8").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais9(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal9").style.display = "flex";
+}
+
+function fecharModal9() {
+  document.getElementById("modal9").style.display = "none";
+}
+
+//----------------------------------
+
+function saibaMais10(event) {
+  event.stopPropagation(); 
+  document.getElementById("modal10").style.display = "flex";
+}
+
+function fecharModal10() {
+  document.getElementById("modal10").style.display = "none";
+}
+
+//----------------------------------
+
+
+
